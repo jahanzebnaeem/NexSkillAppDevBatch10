be26eaa718d3d7847fa876d168247e762bd7b726
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-02 14:29:16 +0500

    Lecture 2
    End of lecture code.

--- calculatordemo/app/src/main/java/com/example/android/calculatordemo/MainActivity.java
+++ calculatordemo/app/src/main/java/com/example/android/calculatordemo/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.android.calculatordemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tvGreeting);
+        textView.setText("Welcome");
     }
 }
