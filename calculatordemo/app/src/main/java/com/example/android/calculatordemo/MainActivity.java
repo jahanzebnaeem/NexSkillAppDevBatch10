9ef0ed430cead0a2a30d54ac18bbbd95fa910aa3
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-09 12:52:59 +0500

    Lecture 4
    Layout.

--- calculatordemo/app/src/main/java/com/example/android/calculatordemo/MainActivity.java
+++ calculatordemo/app/src/main/java/com/example/android/calculatordemo/MainActivity.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.tvGreeting);
-        textView.setText("Welcome");
     }
 }
