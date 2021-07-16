d1c169a3ecf32194c8764c47991cd809f7da06ca
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-16 14:44:11 +0500

    Lecture 6
    Making app safe from hacking + Menu + Localisation.

--- OpenNewScreen/app/src/main/java/com/example/android/opennewscreen/Home.java
+++ OpenNewScreen/app/src/main/java/com/example/android/opennewscreen/Home.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tvName = (TextView) findViewById(R.id.tvName);
+        Bundle bundle = getIntent().getExtras();
+        tvName.setText(bundle.getString("name"));
     }
 
     public void Close(View view) {
--- OpenNewScreen/app/src/main/java/com/example/android/opennewscreen/MainActivity.java
+++ OpenNewScreen/app/src/main/java/com/example/android/opennewscreen/MainActivity.java
@@ -4,18 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName = (EditText) findViewById(R.id.etName);
     }
 
     public void OpenActivity(View view) {
         Intent intent = new Intent(this, Home.class);
+        intent.putExtra("name", etName.getText().toString());
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home:
+                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.plus:
+                Toast.makeText(getApplicationContext(), "Plus", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.setting:
+                Toast.makeText(getApplicationContext(), "Setting", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
